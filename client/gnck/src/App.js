bcd2983e9159da947d4e920173f2efb8b4a7fd59
GreggLedoux <guitlouie@gmail.com>
2023-05-06 13:42:00 -0400

    vercel changes

--- client/gnck/src/App.js
+++ client/gnck/src/App.js
@@ -1,22 +1,40 @@
-
-
-
-//components
-
 import React, { Fragment, useState } from "react"; //groups list of children without adding extra nodes to the DOM
+import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
 import './App.css';
 
-
 //components
-
 import InputTodo from "./components/InputTodo/InputTodo";
 import ListTodos from "./components/ListTodos/ListTodos";
-import CompletedList from "./components/CompletedList/CompletedList";
+import About from './components/About/About'
+import Sidebar from "./components/Sidebar/Sidebar";
 
+function Header() {
+  return (
+    <header>
+      <Container>
+        <Nav.Item>
+          <h1>GNCK List</h1>
+        </Nav.Item>
+        <Nav defaultActiveKey="/" variant="tabs" className="justify-content-center">
+          <Nav.Item>
+            <Nav.Link as={Link} to="/">
+              Todo
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link as={Link} to="/about">
+              About Us
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Container>
+    </header>
+  );
+}
 
-
-
-function App() {
+function Display() {
   const [isCompletedTodoUpdated, setIsCompletedTodoUpdated] = useState(false);
 
   const handleCompletedTodo = () => {
@@ -24,13 +42,32 @@ function App() {
   };
 
   return (
-    <Fragment>
-      <div className="container">
-        <InputTodo />
-        <ListTodos onTodoCompleted={handleCompletedTodo} />
-        <CompletedList isCompletedTodoUpdated={isCompletedTodoUpdated} />
-      </div>
-    </Fragment>
+    <div className="display">
+      <Routes>
+        <Route path="/" element={<Fragment>
+          <InputTodo />
+          <ListTodos onTodoCompleted={handleCompletedTodo} />
+          <Sidebar
+            isCompletedTodoUpdated={isCompletedTodoUpdated}
+            handleCompletedTodo={handleCompletedTodo}
+          />
+        </Fragment>} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <main>
+          <Display />
+        </main>
+      </Router>
+    </div>
   );
 }
 
