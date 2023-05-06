96298832d85c2c20673baf8adf64f9a5166e7db6
Casey Halbohn <wrxbeauty15@hotmail.com>
2023-05-06 10:09:22 -0400

    updated app

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,16 @@
 import React, { Fragment, useState } from "react"; //groups list of children without adding extra nodes to the DOM
+import './App.css';
 import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom'
 import Nav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container'
-import './App.css';
 
 //components
 import InputTodo from "./components/InputTodo/InputTodo";
 import ListTodos from "./components/ListTodos/ListTodos";
-import About from './components/About/About'
 import Sidebar from "./components/Sidebar/Sidebar";
+import About from './components/About/About'
 
-function Header() {
-  return (
-    <header>
-      <Container>
-        <Nav.Item>
-          <h1>GNCK List</h1>
-        </Nav.Item>
-        <Nav defaultActiveKey="/" variant="tabs" className="justify-content-center">
-          <Nav.Item>
-            <Nav.Link as={Link} to="/">
-              Todo
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link as={Link} to="/about">
-              About Us
-            </Nav.Link>
-          </Nav.Item>
-        </Nav>
-      </Container>
-    </header>
-  );
-}
-
-function Display() {
+function App() {
   const [isCompletedTodoUpdated, setIsCompletedTodoUpdated] = useState(false);
 
   const handleCompletedTodo = () => {
@@ -42,31 +18,52 @@ function Display() {
   };
 
   return (
-    <div className="display">
-      <Routes>
-        <Route path="/" element={<Fragment>
-          <InputTodo />
-          <ListTodos onTodoCompleted={handleCompletedTodo} />
-          <Sidebar
-            isCompletedTodoUpdated={isCompletedTodoUpdated}
-            handleCompletedTodo={handleCompletedTodo}
-          />
-        </Fragment>} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </div>
-  );
-}
 
-function App() {
-  return (
     <div className="App">
+
       <Router>
-        <Header />
-        <main>
-          <Display />
-        </main>
+        <header>
+          <Container>
+            <Nav className="justify-content-space-between" defaultActiveKey="/" variant="tabs">
+              <Nav.Item >
+                <Nav.Link href="/">
+                  <Link to="/">GNCK</Link>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item >
+                <Nav.Link eventKey={"aboutPage"}>
+                  <Link to="/about">About Us</Link>
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Container>
+
+        </header>
+
+        <div className="display">
+          <Routes>
+
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
       </Router>
+
+      <body>
+
+        <Fragment>
+          <div className="container">
+            <InputTodo />
+            <ListTodos onTodoCompleted={handleCompletedTodo} />
+            <Sidebar
+              isCompletedTodoUpdated={isCompletedTodoUpdated}
+              handleCompletedTodo={handleCompletedTodo} // Pass the function here
+            />
+
+          </div>
+        </Fragment>
+      
+      </body>
+
     </div>
   );
 }
