e8508cef96214fe96db69560799b2dc0da8d5bf2
Casey Halbohn <wrxbeauty15@hotmail.com>
2023-05-06 13:38:19 -0400

    updated to for deployment

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("./db.js"); // Import the database connection pool from db.js
+const path = require('path');
 
 // Set up middleware
 app.use(cors()); // Enable CORS (Cross-Origin Resource Sharing)
@@ -10,6 +11,7 @@ app.use(express.json()); // Enable JSON parsing for request bodies
 require("dotenv").config(); // Load environment variables from a .env file if it exists
 
 // Define routes for the application
+app.use(express.static(path.resolve(__dirname, '..', 'client/build')))
 
 //Welcome message route
 app.get("/", (req, res) => {
